b47999f47f86318331cabc5cfcb967a12a15b7c1
chris-forbes-1 <christopher.forbes@strath.ac.uk>
2013-01-29 13:46:30 +0000

    last of today in uni more tonight

--- src/com/cs408/forbes/chris/analysis/classes/Multi_word_frequency_analysis.java
+++ src/com/cs408/forbes/chris/analysis/classes/Multi_word_frequency_analysis.java
@@ -1,4 +1,5 @@
 package com.cs408.forbes.chris.analysis.classes;
+import com.cs408.forbes.chris.analysis.types.*;
 /**
  * 
  * @author cforbes2013 <christopher.forbes@strath.ac.uk>
@@ -9,7 +10,11 @@ public class Multi_word_frequency_analysis {
 	
 	public Multi_word_frequency_analysis(String fp)
 	{
-		
+		parse_data(fp);
+	}
+	
+	private static void parse_data(String fp){
+	
 	}
 
 }
